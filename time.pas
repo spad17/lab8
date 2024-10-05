8e8a85ccfb4135264635ca59d660c45966889f7a
Алексей Миргородский <mirgorodsky.alexey@gmail.com>
2024-10-05 06:11:06 +0300

    Edit time.pas

--- time.pas
+++ time.pas
@@ -1,3 +1,18 @@
 ﻿begin
-  
+  var N := ReadInteger('Введите год');
+  Assert(N>0);
+  if N mod 100 = 0 then
+  begin
+    if N mod 400 <> 0 then
+    begin
+      Println($'{N} год не високосный');
+      exit;
+    end
+    else 
+      Println($'{N} год високосный');
+  end
+  else if n mod 4 = 0 then
+    Println($'{N} год високосный')
+  else 
+    Println($'{N} год не високосный');
 end.
