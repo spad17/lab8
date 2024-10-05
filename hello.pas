2d6481e38dbe2b9f1b834c88e2d1a5ea935d92a2
Алексей Миргородский <mirgorodsky.alexey@gmail.com>
2024-10-05 05:41:39 +0300

    Edit hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  
+  var N := ReadInteger('Который сейчас час?');
+  Assert(N >=0);
+  Assert(N<24);
+  case N of 
+  4..10 : Println('Доброе утро, мир!');
+  11..16 : Println('Добрый день, мир!');
+  17..22 : Println('Добрый вечер, мир!');
+  0..3,23 : Println('Доброй ночи, мир!');
+  else Print('Ошибка');
+  end;
 end.
